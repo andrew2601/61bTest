8de9e40df5d7a281876c13e825ff779ced95afab
andrew2601 <andrew2601@gmail.com>
2021-12-21 11:38:34 -0500

    Test

---
+++ lab2/Linkedlist/src/IntNode.java
@@ -0,0 +1,12 @@
+public class IntNode {
+    public int item;
+    public IntNode next;
+
+    public IntNode(int i, IntNode n){
+        item = i;
+        next = n;
+    }
+    public static void main (String[] args){
+        SLList L = new SLList(10);
+    }
+}
